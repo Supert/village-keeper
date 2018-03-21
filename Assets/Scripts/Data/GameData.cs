54bc9923cdaa07bc7982156aed9eab6cd67f94d4
Alexey Korolev <doctor.irrational@gmail.com>
2018-03-21 16:09:55 +0500

    Moved UiData to separate file.

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -3,16 +3,6 @@ using Shibari;
 
 namespace VillageKeeper.Model
 {
-   public class UiData : BindableData
-    {
-        public PauseMenu PauseMenu { get; } = new PauseMenu();
-
-        public HelpMenu HelpMenu { get; } = new HelpMenu();
-
-        public BuildingPicker BuildingPicker { get; } = new BuildingPicker();
-
-        public CastleMenu CastleMenu { get; } = new CastleMenu();
-    }
     public class GameData : BindableData
     {
         public CalculatedValue<int> RoundFinishedBonusGold { get; } = new CalculatedValue<int>(
---
+++ Assets/Scripts/Data/UiData.cs
@@ -0,0 +1,15 @@
+﻿using Shibari;
+
+namespace VillageKeeper.Model
+{
+    public class UiData : BindableData
+    {
+        public PauseMenu PauseMenu { get; } = new PauseMenu();
+
+        public HelpMenu HelpMenu { get; } = new HelpMenu();
+
+        public BuildingPicker BuildingPicker { get; } = new BuildingPicker();
+
+        public CastleMenu CastleMenu { get; } = new CastleMenu();
+    }
+}
