9a579a4100cf7e53ed6452c89daed4069bf42fc2
Alexey Korolev <doctor.irrational@gmail.com>
2018-03-21 20:08:25 +0500

    Submodule removed: it wasn't really handy.

---
+++ Assets/Shibari/Editor/ShibariSettingsWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Shibari.Editor
+{
+    public class ShibariSettingsWindow : EditorWindow
+    {
+        [MenuItem("Shibari/Settings")]
+        private static void GetWindow()
+        {
+            ShibariSettingsWindow window = (ShibariSettingsWindow)GetWindow(typeof(ShibariSettingsWindow));
+            window.Show();
+        }
+
+        void OnGUI()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath<ShibariSettings>(Shibari.Model.SETTINGS_PATH);
+
+            if (prefab == null)
+                Shibari.Model.InitializeSettingsPrefab();
+            prefab = AssetDatabase.LoadAssetAtPath<ShibariSettings>(Shibari.Model.SETTINGS_PATH);
+
+            var editor = UnityEditor.Editor.CreateEditor(prefab);
+
+            if (editor != null)
+            {
+                var so = editor.serializedObject;
+                so.Update();
+                editor.OnInspectorGUI();
+
+                so.ApplyModifiedProperties();
+
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+}
---
+++ Assets/Shibari/Model.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace Shibari
+{
+    [InitializeOnLoad]
+    public static class Model
+    {
+        public const string SETTINGS_PATH = "Assets/Shibari/Resources/ShibariSettings.prefab";
+        public const string SERIALIZATION_TEMPLATES = "Assets/Shibari/Templates/";
+
+        public static BindableData RootNode { get; private set; }
+        public static Type RootNodeType { get; private set; }
+
+        static Model()
+        {
+            DeserializeRootNodeType();
+        }
+
+        public static void InitializeSettingsPrefab()
+        {
+            var settingsPrefab = new GameObject();
+            settingsPrefab.AddComponent<ShibariSettings>();
+            string[] splittedPath = SETTINGS_PATH.Split('/');
+            string builtFolders = splittedPath[0];
+            for (int i = 1; i < splittedPath.Length - 1; i++)
+            {
+                if (!AssetDatabase.GetSubFolders(builtFolders).Contains(builtFolders + "/" + splittedPath[i]))
+                    AssetDatabase.CreateFolder(builtFolders, splittedPath[i]);
+                builtFolders += "/" + splittedPath[i];
+            }
+
+            PrefabUtility.CreatePrefab(SETTINGS_PATH, settingsPrefab);
+        }
+
+        public static void DeserializeRootNodeType()
+        {
+            ShibariSettings settings = Resources.Load<ShibariSettings>("ShibariSettings");
+            if (settings == null)
+                InitializeSettingsPrefab();
+            settings = Resources.Load<ShibariSettings>("ShibariSettings");
+            RootNodeType = settings.RootNodeType.Type;
+            if (RootNodeType == null)
+            {
+                Debug.LogError("Please, set root node type in Shibari/Settings menu.");
+                return;
+            }
+            else if (RootNodeType.GetConstructor(new Type[0]) == null)
+            {
+                Debug.LogError($"Root node type {RootNodeType} should implement default public constructor");
+            }
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            RootNode = (BindableData)Activator.CreateInstance(RootNodeType);
+            RootNode.Initialize();
+        }
+
+        public static IEnumerable<Type> GetBindableDataTypes()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            IEnumerable<Type> result = GetBindableDataTypesInAssembly(executingAssembly);
+
+            result = result.Concat(executingAssembly.GetReferencedAssemblies().SelectMany(assembly => GetBindableDataTypesInAssembly(Assembly.Load(assembly))));
+            return result;
+        }
+
+        public static string GenerateSerializationTemplate(Type t)
+        {
+            if (!typeof(BindableData).IsAssignableFrom(t))
+                throw new ArgumentException("Type t should be child of BindableData", "t");
+
+            return BindableDataJsonConverter.GenerateJsonTemplate(t);
+        }
+
+        public static string GenerateSerializationTemplate<T>() where T : BindableData
+        {
+            return GenerateSerializationTemplate(typeof(T));
+        }
+
+        private static IEnumerable<Type> GetBindableDataTypesInAssembly(Assembly assembly)
+        {
+            return assembly.GetTypes()
+                .Where(t => !t.IsAbstract)
+                .Where(t => typeof(BindableData).IsAssignableFrom(t));
+        }
+    }
+}
